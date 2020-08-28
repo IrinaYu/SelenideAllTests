df2c768e835376432411a24589138da6757a3fa9
IrinaYu <irinayurchenko27@gmail.com>
2020-08-28 23:48:27 +0300

    fixes

--- src/main/java/pages/TicketPage.java
+++ src/main/java/pages/TicketPage.java
@@ -3,7 +3,6 @@ package pages;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 
